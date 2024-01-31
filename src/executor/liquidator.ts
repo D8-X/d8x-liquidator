b4bb30fe831e8782435f07ec25de05f958e21f9f
m66260 <m66260@protonmail.com>
2024-01-31 10:09:57 -0500

    logs

--- src/executor/liquidator.ts
+++ src/executor/liquidator.ts
@@ -8,7 +8,7 @@ import { BotStatus, LiquidateTraderMsg, LiquidatorConfig } from "../types";
 export default class Liquidator {
   // objects
   private providers: providers.StaticJsonRpcProvider[];
-  private liqTool: { api: LiquidatorTool; busy: boolean }[];
+  private bots: { api: LiquidatorTool; busy: boolean }[];
   private redisSubClient: Redis;
 
   // parameters
@@ -20,6 +20,7 @@ export default class Liquidator {
   // state
   private q: Set<string> = new Set();
   private lastLiquidateCall: number = 0;
+  private locked: Set<string> = new Set();
 
   constructor(pkTreasury: string, pkLiquidators: string[], config: LiquidatorConfig) {
     this.treasury = pkTreasury;
@@ -27,7 +28,7 @@ export default class Liquidator {
     this.config = config;
     this.redisSubClient = constructRedis("executorSubClient");
     this.providers = this.config.rpcExec.map((url) => new providers.StaticJsonRpcProvider(url));
-    this.liqTool = this.privateKey.map((pk) => ({
+    this.bots = this.privateKey.map((pk) => ({
       api: new LiquidatorTool(PerpetualDataHandler.readSDKConfig(this.config.sdkConfig), pk),
       busy: false,
     }));
@@ -53,7 +54,7 @@ export default class Liquidator {
       console.log(`trying provider ${i} ... `);
       const results = await Promise.allSettled(
         // createProxyInstance attaches the given provider to the object instance
-        this.liqTool.map((liq) => liq.api.createProxyInstance(this.providers[i]))
+        this.bots.map((liq) => liq.api.createProxyInstance(this.providers[i]))
       );
       success = results.every((r) => r.status === "fulfilled");
       i = (i + 1) % this.providers.length;
@@ -122,6 +123,102 @@ export default class Liquidator {
     });
   }
 
+  private async liquidateTraderByBot(botIdx: number, symbol: string, trader: string) {
+    trader = trader.toLowerCase();
+    if (this.bots[botIdx].busy || this.locked.has(`${symbol}:${trader}`)) {
+      return false;
+    }
+    // lock
+    this.bots[botIdx].busy = true;
+    this.locked.add(`${symbol}:${trader}`);
+
+    // check if order is on-chain - stop if not/unable to check
+    const isMarginSafe = await this.bots[botIdx].api.isMaintenanceMarginSafe(symbol, trader).catch(() => true);
+
+    if (isMarginSafe) {
+      console.log({
+        info: "trader is margin safe",
+        symbol: symbol,
+        executor: this.bots[botIdx].api.getAddress(),
+        trader: trader,
+      });
+      this.bots[botIdx].busy = false;
+      this.locked.delete(`${symbol}:${trader}`);
+      return false;
+    }
+
+    // submit txn
+    console.log({
+      info: "submitting txn...",
+      symbol: symbol,
+      executor: this.bots[botIdx].api.getAddress(),
+      trader: trader,
+    });
+    let tx: ContractTransaction;
+    try {
+      tx = await this.bots[botIdx].api.liquidateTrader(symbol, trader, this.config.rewardsAddress, undefined, {
+        gasLimit: 1_000_000,
+      });
+    } catch (e: any) {
+      console.log({
+        info: "txn rejected",
+        reason: e.toString(),
+        symbol: symbol,
+        executor: this.bots[botIdx].api.getAddress(),
+        trader: trader,
+      });
+      this.bots[botIdx].busy = false;
+      this.locked.delete(`${symbol}:${trader}`);
+      return false;
+    }
+    console.log({
+      info: "txn accepted",
+      symbol: symbol,
+      orderBook: tx.to,
+      executor: tx.from,
+      trader: trader,
+      gasLimit: `${utils.formatUnits(tx.gasLimit, "wei")} wei`,
+      hash: tx.hash,
+    });
+
+    // confirm execution
+    try {
+      const receipt = await tx.wait();
+      console.log({
+        info: "txn confirmed",
+        symbol: symbol,
+        orderBook: receipt.to,
+        executor: receipt.from,
+        trader: trader,
+        block: receipt.blockNumber,
+        gasUsed: `${utils.formatUnits(receipt.cumulativeGasUsed, "wei")} wei`,
+        hash: receipt.transactionHash,
+      });
+      this.locked.delete(`${symbol}:${trader}`);
+    } catch (e: any) {
+      let error = e?.toString() || "";
+      console.log({
+        info: "txn reverted",
+        reason: error,
+        symbol: symbol,
+        executor: this.bots[botIdx].api.getAddress(),
+        trader: trader,
+      });
+      this.locked.delete(`${symbol}:${trader}`);
+      const bot = this.bots[botIdx].api.getAddress();
+      if (error.includes("insufficient funds for intrinsic transaction cost")) {
+        try {
+          await this.fundWallets([bot]);
+        } catch (e: any) {
+          console.log(`failed to fund bot ${bot}`);
+        }
+      }
+    }
+    // unlock bot
+    this.bots[botIdx].busy = false;
+    return true;
+  }
+
   /**
    * Liquidate traders in q
    */
@@ -139,107 +236,47 @@ export default class Liquidator {
       return BotStatus.Ready;
     }
 
-    const txns: { tx: Promise<ContractTransaction>; botIdx: number; symbol: string; trader: string }[] = [];
+    const executed: Promise<boolean>[] = [];
     for (const msg of q) {
-      let assignedTx = false;
-      for (let i = 0; i < this.liqTool.length && !assignedTx; i++) {
-        const liq = this.liqTool[i];
+      const { symbol, traderAddr }: LiquidateTraderMsg = JSON.parse(msg);
+      for (let i = 0; i < this.bots.length; i++) {
+        const liq = this.bots[i];
         if (!liq.busy) {
           // msg will be attempted by this liquidator
           attempts++;
           this.q.delete(msg);
-          liq.busy = true;
-          assignedTx = true;
-          const { symbol, traderAddr }: LiquidateTraderMsg = JSON.parse(msg);
-          txns.push({
-            tx: executeWithTimeout(
-              liq.api.liquidateTrader(symbol, traderAddr, this.earnings, undefined, {
-                gasLimit: 1_000_000,
-              }),
-              30_000,
-              "timeout"
-            ),
-            botIdx: i,
-            symbol: symbol,
-            trader: traderAddr,
-          });
+          executed.push(this.liquidateTraderByBot(i, symbol, traderAddr));
         }
-      }
-      if (!assignedTx) {
-        // all busy
-        break;
       }
     }
     // send txns
-    const results = await Promise.allSettled(txns.map(({ tx }) => tx));
-
-    const confirmations: Promise<void>[] = [];
+    const results = await Promise.allSettled(executed);
     for (let i = 0; i < results.length; i++) {
       const result = results[i];
       if (result.status === "fulfilled") {
-        successes++;
-        console.log({
-          symbol: txns[i].symbol,
-          orderBook: result.value.to,
-          liquidator: result.value.from,
-          trader: txns[i].trader,
-          txn: result.value.hash,
-        });
-        confirmations.push(
-          executeWithTimeout(
-            result.value.wait().then(() => {
-              this.liqTool[txns[i].botIdx].busy = false;
-            }),
-            30_000,
-            "timeout"
-          )
-        );
+        successes += result.value ? 1 : 0;
       } else {
-        const liq = this.liqTool[txns[i].botIdx].api.getAddress();
-        let prom: Promise<void>;
-        const error = result.reason.toString();
-        if (error.includes("insufficient funds for intrinsic transaction cost")) {
-          prom = this.fundWallets([liq]);
-        } else {
-          if (!error.includes("gas price too low")) {
-            console.log(`${liq} failed with reason ${result.reason}`);
-          }
-          prom = Promise.resolve();
-        }
-        confirmations.push(
-          prom.then(() => {
-            this.liqTool[txns[i].botIdx].busy = false;
-          })
-        );
+        console.log(`uncaught error: ${result.reason.toString()}`);
       }
     }
 
     // return cases:
-    let res: BotStatus;
-    if (successes == 0 && attempts == this.liqTool.length) {
+    if (successes == 0 && attempts == this.bots.length) {
       // all bots are down, either rpc or px service issue
-      res = BotStatus.Error;
+      return BotStatus.Error;
     } else if (attempts == 0 && q.length > 0) {
       // did not try anything
-      res = BotStatus.Busy;
+      return BotStatus.Busy;
     } else if (successes == 0 && attempts > 0) {
       // tried something but it didn't work
-      res = BotStatus.PartialError;
+      return BotStatus.PartialError;
     } else if (successes < attempts) {
       // some attempts worked, others failed
-      res = BotStatus.PartialError;
+      return BotStatus.PartialError;
     } else {
       // everything worked or nothing happend
-      if (attempts > 0) {
-      }
-      res = BotStatus.Ready;
+      return BotStatus.Ready;
     }
-
-    (await Promise.allSettled(confirmations)).map((_result, i) => {
-      this.liqTool[txns[i].botIdx].busy = false;
-    });
-
-    return res;
   }
 
   public async fundWallets(addressArray: string[]) {
