151551c65350ee3b717ed522706e3591b967f04f
m66260 <m66260@protonmail.com>
2025-02-17 22:20:01 -0500

    expose errors

--- src/multiUrlJsonRpcProvider.ts
+++ src/multiUrlJsonRpcProvider.ts
@@ -169,7 +169,7 @@ export class MultiUrlJsonRpcProvider extends JsonRpcApiProvider implements Multi
         );
       }
       this.switchRpcOnError();
-      return this._send(payload);
+      // return this._send(payload);
     }
 
     // Request was sent successfuly, reset errors counter
